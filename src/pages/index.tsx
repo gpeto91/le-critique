440090b07f1d3e69743e4d2f20b00a00374f1610
Gabriel Azevedo <gabriel.azevedo@guaranisistemas.com.br>
2022-02-10 16:51:48 -0300

    remover getServerSideProps da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,12 @@
-import { getProviders } from 'next-auth/react'
-import { useContext, useEffect } from 'react'
 import { FilmCard } from '../components/FilmCard'
-
-import AppContext, { IProvider } from '../context/AppProvider'
 
 import common from "../../styles/common.module.scss"
 import styles from './home.module.scss'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
-type HomeProps = {
-  providers: IProvider[];
-}
-
-const Home = ({ providers }: HomeProps) => {
-  const { state, setState } = useContext(AppContext)
+const Home = () => {
   const router = useRouter()
-
-  useEffect(() => {
-    setState({
-      ...state,
-      providers,
-    })
-  }, [])
 
   return (
     <>
@@ -44,17 +28,3 @@ const Home = ({ providers }: HomeProps) => {
 }
 
 export default Home
-
-export async function getServerSideProps() {
-  const providers = await getProviders()
-
-  const formatedProviders = providers && Object.values(providers).map(provider => ({
-    id: provider.id,
-    name: provider.name,
-    type: provider.type
-  }))
-
-  return {
-    props: { providers: formatedProviders }
-  }
-}
